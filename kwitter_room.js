7da32bce1eedcdee0228316ebe8c238ce1ba22ac
Amaey Mittal <100258151+amaeymittal@users.noreply.github.com>
2022-08-27 10:51:20 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCOQiTaVL1oSKIEW-kvheqrbUrJcnHlVhA",
+    authDomain: "projectdb-c0486.firebaseapp.com",
+    databaseURL: "https://projectdb-c0486-default-rtdb.firebaseio.com",
+    projectId: "projectdb-c0486",
+    storageBucket: "projectdb-c0486.appspot.com",
+    messagingSenderId: "181621866168",
+    appId: "1:181621866168:web:b145a1d5a1c93a225c7e4d"
+  };
+  
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
